ed55954d7c160a9a19d956aef255d707aff93eba
Adrian Siekierka <kontakt@asie.pl>
2023-03-23 21:15:10 +0100

    libc, ndsabi: minor improvements

--- source/arm9/libc_private.h
+++ source/arm9/libc_private.h
@@ -2,16 +2,12 @@
 //
 // Copyright (c) 2023 Antonio Niño Díaz
 
-#ifndef LIBC_PRIVATE_H__
-#define LIBC_PRIVATE_H__
+#ifndef ARM9_LIBC_PRIVATE_H__
+#define ARM9_LIBC_PRIVATE_H__
 
-#include <time.h>
-
-void __libnds_exit(int rc);
+#include "../common/libc_private.h"
 
 typedef ssize_t (* fn_write_ptr)(const char *, size_t);
 extern fn_write_ptr libnds_stdout_write, libnds_stderr_write;
 
-extern time_t *punixTime;
-
-#endif // LIBC_PRIVATE_H__
+#endif // ARM9_LIBC_PRIVATE_H__
