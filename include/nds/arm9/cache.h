a4f249081a68009f8aecc15e561e35a0eda41736
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-04-08 02:32:55 +0100

    cache: Cleanup compatibility header
    It's better to have static inline functions than defines so that the
    error codes of the compiler and linker make more sense to the user.

--- include/nds/arm9/cache.h
+++ include/nds/arm9/cache.h
@@ -26,10 +26,11 @@
 		distribution.
 
 ---------------------------------------------------------------------------------*/
-/*! \file cache.h
-	\brief ARM9 cache control functions.
-*/
-#ifndef	_cache_h_
+
+/// @file cache.h
+/// @brief ARM9 cache control functions.
+
+#ifndef _cache_h_
 #define _cache_h_
 
 #ifdef __cplusplus
@@ -40,46 +41,49 @@ extern "C" {
 
 #include "nds/arm9/cp15.h"
 
-/*! \fn IC_InvalidateAll()
-	\brief invalidate entire instruction cache.
-*/
-#define IC_InvalidateAll CP15_FlushICache
+/// Invalidate the entire instruction cache.
+static inline void IC_InvalidateAll(void)
+{
+    CP15_FlushICache();
+}
 
+/// Invalidate the instruction cache of a range of addresses.
+///
+/// @param base Base address of the region.
+/// @param size Size of the region.
+static inline void IC_InvalidateRange(const void *base, u32 size)
+{
+    CP15_FlushIcacheRange(base, size);
+}
 
-/*! \fn IC_InvalidateRange(const void *base, u32 size)
-	\brief invalidate the instruction cache for a range of addresses.
-	\param base base address of the region to invalidate
-	\param size size of the region to invalidate.
-*/
-#define IC_InvalidateRange CP15_FlushIcacheRange
+/// Flush the entire data cache to memory.
+static inline void DC_FlushAll(void)
+{
+    CP15_CleanAndFlushDcache();
+}
 
+/// Flush the data cache of a range of addresses to memory.
+///
+/// @param base Base address of the region.
+/// @param size Size of the region.
+static inline void DC_FlushRange(const void *base, u32 size)
+{
+    CP15_CleanAndFlushDcacheRange(base, size);
+}
 
-/*! \fn DC_FlushAll()
-	\brief flush the entire data cache to memory.
-*/
-#define DC_FlushAll CP15_CleanAndFlushDcache
+/// Invalidate the entire data cache.
+static inline void DC_InvalidateAll(void)
+{
+    CP15_FlushDCache();
+}
 
-
-/*! \fn DC_FlushRange(const void *base, u32 size)
-	\brief flush the data cache for a range of addresses to memory.
-	\param base base address of the region to flush.
-	\param size size of the region to flush.
-*/
-#define DC_FlushRange CP15_CleanAndFlushDcacheRange
-
-
-/*! \fn DC_InvalidateAll()
-	\brief invalidate the entire data cache.
-*/
-#define DC_InvalidateAll CP15_FlushDCache
-
-
-/*! \fn DC_InvalidateRange(const void *base, u32 size)
-	\brief invalidate the data cache for a range of addresses.
-	\param base base address of the region to invalidate
-	\param size size of the region to invalidate.
-*/
-#define DC_InvalidateRange CP15_FlushDcacheRange
+/// Invalidate the data cache of a range of addresses.
+/// @param base base address of the region to invalidate
+/// @param size size of the region to invalidate.
+static inline void DC_InvalidateRange(const void *base, u32 size)
+{
+    CP15_FlushDcacheRange(base, size);
+}
 
 #ifdef __cplusplus
 }
