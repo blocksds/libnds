53b83b2be4f7767ab969e42f7fd2581252d21e1c
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-04-16 02:51:42 +0100

    reset: Fix reset functions
    They were both using the commands used before the FIFO refactor.

--- source/common/reset.c
+++ source/common/reset.c
@@ -1,27 +1,38 @@
 // SPDX-License-Identifier: Zlib
 //
 // Copyright (C) 2010 Dave Murphy (WinterMute)
+// Copyright (c) 2023 Antonio Niño Díaz
 
 #include <nds/ndstypes.h>
 #include <nds/ipc.h>
 #include <nds/system.h>
 
-#ifdef ARM7
+#include "fifo_ipc_messages.h"
 
-void resetARM9(u32 address) {
-	*((vu32*)0x02FFFE24) = address;
-
-#else
-
-void resetARM7(u32 address) {
-	*((vu32*)0x02FFFE34) = address;
-
-#endif
-
-	REG_IPC_FIFO_TX = 0x0c04000c;
-	while((REG_IPC_SYNC & 0x0f) != 1);
-	REG_IPC_SYNC = 0x100;
-	while((REG_IPC_SYNC & 0x0f) != 0);
-	REG_IPC_SYNC = 0;
+static void resetSync(void)
+{
+    while ((REG_IPC_SYNC & 0x0f) != 1);
+    REG_IPC_SYNC = 0x100;
+    while ((REG_IPC_SYNC & 0x0f) != 0);
+    REG_IPC_SYNC = 0;
 }
 
+#ifdef ARM7
+void resetARM9(u32 address)
+{
+    *((vu32 *)0x02FFFE24) = address;
+
+    REG_IPC_FIFO_TX = FIFO_ADDRESSBIT | FIFO_IMMEDIATEBIT
+                    | FIFO_ARM7_REQUESTS_ARM9_RESET;
+    resetSync();
+}
+#else
+void resetARM7(u32 address)
+{
+    *((vu32 *)0x02FFFE34) = address;
+
+    REG_IPC_FIFO_TX = FIFO_ADDRESSBIT | FIFO_IMMEDIATEBIT
+                    | FIFO_ARM9_REQUESTS_ARM7_RESET;
+    resetSync();
+}
+#endif
