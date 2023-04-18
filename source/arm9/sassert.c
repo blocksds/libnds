7e8f229384d68007cf580122996a0e2772bb147f
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-04-18 18:19:45 +0100

    sassert: Improve message format and allow exit to loader

--- source/arm9/sassert.c
+++ source/arm9/sassert.c
@@ -2,34 +2,61 @@
 //
 // Copyright (C) 2013 Jason Rogers (Dovoto)
 // Copyright (C) 2013 Michael Theall (mtheall)
+// Copyright (C) 2023 Antonio Niño Díaz
 
-// Definitons for DS assertions
+// Simple routine to display assertion failure messages.
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <nds/arm9/console.h>
+#include <nds/arm9/input.h>
+#include <nds/input.h>
+#include <nds/interrupts.h>
 
-#include <stdio.h>
-#include <stdarg.h>
-
-void __sassert(const char *fileName, int lineNumber, const char* conditionString, const char* format, ...)
+void __sassert(const char *fileName, int lineNumber, const char *conditionString,
+               const char *format, ...)
 {
     va_list ap;
 
     consoleDemoInit();
 
-    printf("\x1b[j"               /* clear screen */
-           "\x1b[42mAssertion!\n" /* print in green? */
-           "\x1b[39mFile: \n"     /* print in default color */
-           "%s\n\n"               /* print filename */
-           "Line: %d\n\n"         /* print line number */
+    printf("\x1b[2J"                // Clear screen
+           "\x1b[43mAssertion!\n\n" // Print in yellow
+           "\x1b[39mFile: \n"       // Print in default color
+           "%s\n\n"                 // Print filename
+           "Line: %d\n\n"           // Print line number
            "Condition:\n"
-           "%s\n\n"               /* print condition message */
-           "\x1b[41m",            /* change font color to red */
+           "%s\n\n"                 // Print condition message
+           "Message:\n",
            fileName, lineNumber, conditionString);
 
     va_start(ap, format);
     vprintf(format, ap);
     va_end(ap);
 
-    //todo: exit properly
-    while(1);
+    printf("\x1b[39m"); // Print in default color
+
+    printf("\x1b[23;0HPress START to exit");
+
+    while (1)
+    {
+        swiWaitForVBlank();
+        scanKeys();
+        if (keysHeld() & KEY_START)
+            break;
+    }
+
+    // Print an error message over the previous message. This shouldn't normally
+    // be seen by the user because exit() is called right afterwards. It will
+    // only be seen if exit() hangs, which is not its normal behaviour (it
+    // should power off the NDS if it fails to exit to the loader).
+    printf("\x1b[23;0HFailed to exit     ");
+
+    // Return an error code to the loader
+    exit(-1);
+
+    while (1)
+        swiWaitForVBlank();
 }
