5950adabe087701113758b3048b218b035b55cc7
Adrian Siekierka <kontakt@asie.pl>
2023-10-19 06:56:54 +0200

    syscalls: Fix sbrk unification

--- source/common/libc/sbrk.c
+++ source/common/libc/sbrk.c
@@ -6,8 +6,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef ARM9
+#define USE_FAKE_HEAP_BOUNDS
+#endif
+
+#ifdef USE_FAKE_HEAP_BOUNDS
 void *fake_heap_end = NULL;
 void *fake_heap_start = NULL;
+#endif
 
 void *sbrk(int incr)
 {
@@ -21,16 +27,13 @@ void *sbrk(int incr)
     // Next address to be used. It is updated after every call to sbrk()
     static uintptr_t heap_start = 0;
 
+#ifdef USE_FAKE_HEAP_BOUNDS
     if (heap_start == 0)
     {
-#ifdef ARM9
         if (fake_heap_start == NULL)
             heap_start = end;
         else
             heap_start = (uintptr_t)fake_heap_start;
-#else
-        heap_start = end;
-#endif
     }
 
     // Current limit
@@ -40,6 +43,13 @@ void *sbrk(int incr)
         heap_end = stack_ptr;
     else
         heap_end = (uintptr_t)fake_heap_end;
+#else
+    if (heap_start == 0)
+        heap_start = end;
+
+    // Current limit
+    uintptr_t heap_end = stack_ptr;
+#endif
 
     // Try to allocate
     if (heap_start + incr > heap_end)
