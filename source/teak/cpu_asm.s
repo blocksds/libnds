3cd7bcbffbfed467a4fcdac837d941459bc227f7
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-11-10 18:54:47 +0000

    libteak: Cleanup some assembly functions

--- source/teak/cpu_asm.s
+++ source/teak/cpu_asm.s
@@ -1,55 +1,56 @@
 // SPDX-License-Identifier: Zlib
 //
 // Copyright (C) 2020 Gericom
+// Copyright (C) 2023 Antonio Niño Díaz
 
-.text
+#include <teak/asminc.h>
 
-.global cpuDisableIrqs
-cpuDisableIrqs:
+BEGIN_ASM_FUNC cpuDisableIrqs
+
     dint
-    ret always
+    ret     always
 
-.global cpuEnableIrqs
-cpuEnableIrqs:
+BEGIN_ASM_FUNC cpuEnableIrqs
+
     eint
-    ret always
+    ret     always
 
-.global cpuDisableInt0
-cpuDisableInt0:
-    rst 0x100, mod3
-    ret always
+BEGIN_ASM_FUNC cpuDisableInt0
 
-.global cpuEnableInt0
-cpuEnableInt0:
-    set 0x100, mod3
-    ret always
+    rst     0x100, mod3
+    ret     always
 
-.global cpuDisableInt1
-cpuDisableInt1:
-    rst 0x200, mod3
-    ret always
+BEGIN_ASM_FUNC cpuEnableInt0
 
-.global cpuEnableInt1
-cpuEnableInt1:
-    set 0x200, mod3
-    ret always
+    set     0x100, mod3
+    ret     always
 
-.global cpuDisableInt2
-cpuDisableInt2:
-    rst 0x400, mod3
-    ret always
+BEGIN_ASM_FUNC cpuDisableInt1
 
-.global cpuEnableInt2
-cpuEnableInt2:
-    set 0x400, mod3
-    ret always
+    rst     0x200, mod3
+    ret     always
 
-.global cpuDisableVInt
-cpuDisableVInt:
-    rst 0x800, mod3
-    ret always
+BEGIN_ASM_FUNC cpuEnableInt1
 
-.global cpuEnableVInt
-cpuEnableVInt:
-    set 0x800, mod3
-    ret always
+    set     0x200, mod3
+    ret     always
+
+BEGIN_ASM_FUNC cpuDisableInt2
+
+    rst     0x400, mod3
+    ret     always
+
+BEGIN_ASM_FUNC cpuEnableInt2
+
+    set     0x400, mod3
+    ret     always
+
+BEGIN_ASM_FUNC cpuDisableVInt
+
+    rst     0x800, mod3
+    ret     always
+
+BEGIN_ASM_FUNC cpuEnableVInt
+
+    set     0x800, mod3
+    ret     always
