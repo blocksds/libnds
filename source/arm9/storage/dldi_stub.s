9120a00763082a3d9bf3879219b1f537dfdfe480
Adrian Siekierka <kontakt@asie.pl>
2023-11-13 18:34:09 +0100

    dldi_stub: Adjust to crt0 changes.

--- source/arm9/storage/dldi_stub.s
+++ source/arm9/storage/dldi_stub.s
@@ -12,10 +12,7 @@
     .arm
 
     .global _io_dldi_stub
-
-// Total space allocated for user DLDI drivers
-
-    .equ DLDI_ALLOCATED_SPACE, 16384
+    .section .dldi, "ax"
 
 _io_dldi_stub:
 
@@ -23,12 +20,12 @@ dldi_start:
 
 // Driver patch file standard header (16 bytes)
 
-    .word   0xBF8DA5ED      // Magic number to identify this region
-    .asciz  " Chishm"       // Identifying Magic string (8 bytes with null terminator)
-    .byte   0x01            // Version number
-    .byte   DLDI_SIZE_16KB  // 16KiB | Log [base-2] of the size of this driver in bytes.
-    .byte   0x00            // Sections to fix
-    .byte   DLDI_SIZE_16KB  // 16KiB | Log [base-2] of the allocated space in bytes.
+    .word   0xBF8DA5ED        // Magic number to identify this region
+    .asciz  " Chishm"         // Identifying Magic string (8 bytes with null terminator)
+    .byte   0x01              // Version number
+    .byte   __dldi_log2_size  // Log [base-2] of the size of this driver in bytes.
+    .byte   0x00              // Sections to fix
+    .byte   __dldi_log2_size  // 16KiB | Log [base-2] of the allocated space in bytes.
 
 // Text identifier. Up to 47 chars + terminating null (48 bytes)
 
@@ -38,8 +35,8 @@ dldi_start:
 // Offsets to important sections within the data (32 bytes)
 
     .align  6
-    .word   dldi_start  // data start
-    .word   dldi_end    // data end
+    .word   dldi_start    // data start
+    .word   dldi_data_end // data end
     .word   0x00000000  // Interworking glue start -- Needs address fixing
     .word   0x00000000  // Interworking glue end
     .word   0x00000000  // GOT start               -- Needs address fixing
@@ -72,9 +69,4 @@ _DLDI_shutdown:
     .pool
 
 dldi_data_end:
-
-// Pad to end of allocated space
-.space DLDI_ALLOCATED_SPACE - (dldi_data_end - dldi_start)
-
-dldi_end:
     .end
