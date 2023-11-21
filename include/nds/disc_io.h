10ed6fbd7ba63a88e0ea7b1fca036b81c38f20a0
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-11-21 14:34:06 +0000

    chore: Restore SPDX header notices in DLDI files
    Now that the licenses of the files have changed, go back to SPDX.

--- include/nds/disc_io.h
+++ include/nds/disc_io.h
@@ -1,30 +1,10 @@
-/*------------------------------------------------------------------------------
+// SPDX-License-Identifier: Zlib
+// SPDX-FileNotice: Modified from the original version by the BlocksDS project.
+//
+// Copyright (c) 2006 Michael Chisholm (Chishm) and Tim Seidel (Mighty Max).
 
-disc_io.h
-Interface template for low level disc functions.
-
-Copyright (c) 2006 Michael Chisholm (Chishm) and Tim Seidel (Mighty Max).
-
-This software is provided 'as-is', without any express or implied
-warranty.  In no event will the authors be held liable for any damages
-arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-1.	The origin of this software must not be misrepresented; you must not
-	claim that you wrote the original software. If you use this software
-	in a product, an acknowledgment in the product documentation would be
-	appreciated but is not required.
-2.	Altered source versions must be plainly marked as such, and must not be
-	misrepresented as being the original software.
-3.	This notice may not be removed or altered from any source distribution.
-
-------------------------------------------------------------------------------*/
-
-#ifndef NDS_DISC_IO_INCLUDE
-#define NDS_DISC_IO_INCLUDE
+#ifndef LIBNDS_NDS_DISC_IO_H__
+#define LIBNDS_NDS_DISC_IO_H__
 
 #ifdef __cplusplus
 extern "C" {
