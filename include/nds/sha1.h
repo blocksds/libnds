df6b05d903cfea8063c41b68facf4f3829ef775d
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-02-16 12:29:22 +0000

    sha1: Document some missing fields

--- include/nds/sha1.h
+++ include/nds/sha1.h
@@ -20,11 +20,12 @@ extern "C" {
 
 #include <nds/ndstypes.h>
 
+/// Context that holds information about an active SHA1 calculation.
 typedef struct swiSHA1context {
     u32 state[5];       ///< Intermediate digest state
     u32 total[2];       ///< Number of bytes processed
     u8  buffer[64];     ///< Data block being processed
-    u32 fragment_size;
+    u32 fragment_size;  ///< Incomplete fragment size
     void (*sha_block)(struct swiSHA1context *ctx, const void *src, size_t len); ///< Data block being processed
 } swiSHA1context_t;
 
