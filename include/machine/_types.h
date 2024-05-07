c016eacb5917c081970972adba0582d6bcbe1b54
Adrian Siekierka <kontakt@asie.pl>
2024-05-07 17:19:54 +0200

    include: rewrite sys/dirent.h, fix minor issues

---
+++ include/machine/_types.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (c) 2024 Adrian "asie" Siekierka
+
+#ifndef MACHINE__TYPES_H__
+#define MACHINE__TYPES_H__
+
+// Define some platform-specific types for newlib/picolibc.
+
+#include <stdint.h>
+#include <machine/_default_types.h>
+
+typedef uint32_t __ino_t;
+typedef uint64_t __ino64_t;
+#define __machine_ino_t_defined
+
+typedef int32_t _off_t;
+#define __machine_off_t_defined
+
+#endif
--- include/sys/dirent.h
+++ include/sys/dirent.h
@@ -1,12 +1,6 @@
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: Zlib
 //
-// Copyright 2007
-// International Business Machines Corporation,
-// Sony Computer Entertainment, Incorporated,
-// Toshiba Corporation,
-// All rights reserved.
-//
-// Copyright (c) 2023 Antonio Niño Díaz
+// Copyright (c) 2024 Adrian "asie" Siekierka
 
 #ifndef SYS_DIRENT_H__
 #define SYS_DIRENT_H__
@@ -17,36 +11,61 @@ extern "C" {
 
 #include <sys/types.h>
 
-#define MAXNAMLEN   255
-
-#define DT_UNKNOWN  0
-#define DT_FIFO     1
-#define DT_CHR      2
-#define DT_DIR      3
-#define DT_BLK      4
-#define DT_REG      5
-#define DT_LNK      6
-#define DT_SOCK     7
-#define DT_WHT      8
+/// UTF-8 necessitates a maximum of three bytes for any UTF-16 codepoint.
+#define MAXNAMLEN (255 * 3)
 
 struct dirent {
-    ino_t           d_ino;    // Inode number
-    off_t           d_off;    // Value that would be returned by telldir()
-    unsigned short  d_reclen; // Length of this record
-    unsigned char   d_type;   // Type of file; not supported by all filesystems
-    char            d_name[MAXNAMLEN + 1]; // Null-terminated filename
+    /// Inode number. Implementation-defined.
+    ///
+    /// For FAT filesystems, this stores the cluster the file is located on.
+    /// For NitroFS filesystems, this stores the ID of the file.
+    ino_t d_ino;
+
+    /// Index within the directory.
+    off_t d_off;
+
+    /// File/directory name.
+    char d_name[MAXNAMLEN + 1];
+
+    /// File/directory type.
+    ///
+    /// For BlocksDS, this will typically be either DT_REG (file), or DT_DIR
+    /// (directory).
+    unsigned char d_type;
+
+    /// Size of this directory entry.
+    unsigned short d_reclen;
 };
 
 typedef struct {
-    // Private pointer to internal state of the directory.
-    void    *dp;
-    // Type of the private pointer.
-    int     dptype;
-    // Index of the current entry (for telldir() and seekdir()).
-    int     index;
-    // Allow one readdir for each opendir, and store the data here.
-    struct  dirent dirent;
+    // Buffer containing the returned directory entry.
+    struct dirent dirent;
+
+    // Pointer to native directory structure.
+    void *dp;
+
+    // Index within the directory.
+    off_t index;
+
+    // Type of native directory structure pointer.
+    uint8_t dptype;
 } DIR;
+
+/// Unknown file type.
+#define DT_UNKNOWN 0
+
+/// Regular file.
+#define DT_REG 1
+
+/// Directory.
+#define DT_DIR 2
+
+// The following are not used by BlocksDS, but provided for compatibility.
+#define DT_FIFO 3
+#define DT_CHR 4
+#define DT_BLK 5
+#define DT_LNK 6
+#define DT_SOCK 7
 
 #ifdef __cplusplus
 }
