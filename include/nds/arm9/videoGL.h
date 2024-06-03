f8488b70bf46ed3964da545721f62e5ce77783fe
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-06-03 22:49:45 +0100

    videoGL: Add additional note to GL_RGB format documentation

--- include/nds/arm9/videoGL.h
+++ include/nds/arm9/videoGL.h
@@ -258,7 +258,11 @@ typedef enum {
     GL_COMPRESSED = 5, ///< Compressed texture
     GL_RGB8_A5    = 6, ///< 8 color palette, 5 bits of alpha
     GL_RGBA       = 7, ///< 15 bit direct color, 1 bit of alpha
-    GL_RGB        = 8  ///< 15 bit direct color. Converted to GL_RGBA internally, which causes a performance penalty when using glTexImage2D().
+
+    /// 15 bit direct color. Converted to GL_RGBA internally, which causes a
+    /// performance penalty when using glTexImage2D(). This format isn't
+    /// recommended, use GL_RGBA instead.
+    GL_RGB        = 8
 } GL_TEXTURE_TYPE_ENUM;
 
 /// Enums for texture palette data retrieval
