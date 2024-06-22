331f8f6fb4267f7c937414dd0f79d6ad1f126ebb
Adrian Siekierka <kontakt@asie.pl>
2024-06-22 09:55:10 +0200

    include: add shim for nds/arm9/decompress.h

---
+++ include/nds/arm9/decompress.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (c) 2024 Adrian "asie" Siekierka
+
+#if _BLOCKSDS_STRICT_ >= 10300 /* 1.3.0+ */
+#error This include file is no longer supported. Use <nds/decompress.h> instead.
+#endif
+
+#include <nds/decompress.h>
