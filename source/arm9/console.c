blocksds/libnds

commit e70ec2576779f02caa47811a087d719b3b2c56de

console: clean up consoleLoadFont()
- Remove the convertSingleColor option. This should be done by your preferred
  asset conversion tool, such as Grit.
- Use BIOS bit unpack routines instead of a custom one to handle font
  decompression.
- Always initialize the palette when an user palette is not provided.
- Add partial support for 2bpp fonts.

source/arm9/console.c
@@ -11,6 +11,7 @@
 #include <nds/arm9/background.h>
 #include <nds/arm9/console.h>
 #include <nds/arm9/video.h>
+#include <nds/bios.h>
 #include <nds/debug.h>
 #include <nds/memory.h>
 #include <nds/ndstypes.h>
@@ -27,8 +28,7 @@ PrintConsole defaultConsole =
         0,                        // font color count
         1,                        // bpp
         32,                       // first ascii character in the set
-        96,                       // number of characters in the font set
-        true                      // convert single color
+        96                        // number of characters in the font set
     },
     0,  // font background map
     0,  // font background gfx
@@ -324,69 +324,49 @@ void consoleLoadFont(PrintConsole *console)
     // Check which display is being utilized
     if (console->fontBgGfx < BG_GFX_SUB)
         palette = BG_PALETTE;
+    
+    console->fontCurPal <<= 12;
 
-    if (console->font.bpp == 1 || console->font.bpp == 4)
+    if (console->font.bpp <= 2)
     {
-        if (console->font.bpp == 4 && !console->font.convertSingleColor)
+        TUnpackStruct config = {
+            console->font.numChars * 8 * console->font.bpp,
+            console->font.bpp,
+            4,
+            16 - (1 << console->font.bpp)
+        };
+
+        swiUnpackBits(console->font.gfx, console->fontBgGfx, &config);
+    }
+    else if (console->font.bpp == 4)
+    {
+        if (console->font.gfx)
+            dmaCopy(console->font.gfx, console->fontBgGfx,
+                    console->font.numChars * 64 / 2);
+    }
+    else if (console->font.bpp == 8)
+    {
+        if (console->font.gfx)
+            dmaCopy(console->font.gfx, console->fontBgGfx,
+                    console->font.numChars * 64);
+
+        console->fontCurPal = 0;
+    }
+
+    if (console->font.pal)
+    {
+        // Use user-provided palette
+        dmaCopy(console->font.pal, palette + (console->fontCurPal >> 8),
+                    console->font.numColors * 2);
+    }
+    else
+    {
+        // Set default palette (4bpp and 8bpp variants)
+        palette[0] = RGB15(0, 0, 0);
+        palette[16 * 16 - 1] = RGB15(31, 31, 31); // 47 & 39 bright white
+
+        if (console->font.bpp <= 4)
         {
-            if (console->font.gfx)
-                dmaCopy(console->font.gfx, console->fontBgGfx,
-                        console->font.numChars * 64 / 2);
-            if (console->font.pal)
-                dmaCopy(console->font.pal, palette + console->fontCurPal * 16,
-                        console->font.numColors * 2);
-
-            console->fontCurPal <<= 12;
-        }
-        else
-        {
-            console->fontCurPal = 15 << 12;
-
-            if (console->font.bpp == 1)
-            {
-                for (int i = 0; i < console->font.numChars * 8; i++)
-                {
-                    u8 row = ((const u8 *)console->font.gfx)[i];
-                    u32 temp = 0;
-                    if (row & 0x01)
-                        temp |= 0xF;
-                    if (row & 0x02)
-                        temp |= 0xF0;
-                    if (row & 0x04)
-                        temp |= 0xF00;
-                    if (row & 0x08)
-                        temp |= 0xF000;
-                    if (row & 0x10)
-                        temp |= 0xF0000;
-                    if (row & 0x20)
-                        temp |= 0xF00000;
-                    if (row & 0x40)
-                        temp |= 0xF000000;
-                    if (row & 0x80)
-                        temp |= 0xF0000000;
-                    ((u32 *)console->fontBgGfx)[i] = temp;
-                }
-            }
-            else
-            {
-                for (int i = 0; i < console->font.numChars * 16; i++)
-                {
-                    u16 temp = 0;
-
-                    if (console->font.gfx[i] & 0xF)
-                        temp |= 0xF;
-                    if (console->font.gfx[i] & 0xF0)
-                        temp |= 0xF0;
-                    if (console->font.gfx[i] & 0xF00)
-                        temp |= 0xF00;
-                    if (console->font.gfx[i] & 0xF000)
-                        temp |= 0xF000;
-
-                    console->fontBgGfx[i] = temp;
-                }
-            }
-
-            // set up the palette for color printing
             palette[1 * 16 - 1] = RGB15(0, 0, 0);   // 30 normal black
             palette[2 * 16 - 1] = RGB15(15, 0, 0);  // 31 normal red
             palette[3 * 16 - 1] = RGB15(0, 15, 0);  // 32 normal green
@@ -405,47 +385,11 @@ void consoleLoadFont(PrintConsole *console)
             palette[13 * 16 - 1] = RGB15(0, 0, 31);   // 44 bright blue
             palette[14 * 16 - 1] = RGB15(31, 0, 31);  // 45 bright magenta
             palette[15 * 16 - 1] = RGB15(0, 31, 31);  // 46 bright cyan
-            palette[16 * 16 - 1] = RGB15(31, 31, 31); // 47 & 39 bright white
-        }
-    }
-    else if (console->font.bpp == 8)
-    {
-        console->fontCurPal = 0;
 
-        if (!console->font.convertSingleColor)
-        {
-            if (console->font.gfx)
-            {
-                dmaCopy(console->font.gfx, console->fontBgGfx,
-                        console->font.numChars * 64);
-            }
-
-            if (console->font.pal)
-                dmaCopy(console->font.pal, palette, console->font.numColors * 2);
-        }
-        else
-        {
-            for (int i = 0; i < console->font.numChars * 16; i++)
-            {
-                u32 temp = 0;
-
-                if (console->font.gfx[i] & 0xF)
-                    temp = 255;
-                if (console->font.gfx[i] & 0xF0)
-                    temp |= 255 << 8;
-                if (console->font.gfx[i] & 0xF00)
-                    temp |= 255 << 16;
-                if (console->font.gfx[i] & 0xF000)
-                    temp |= 255 << 24;
-
-                ((u32 *)console->fontBgGfx)[i] = temp;
-            }
-
-            palette[255] = RGB15(31, 31, 31);
+            console->fontCurPal = 15 << 12;
         }
     }
 
-    palette[0] = RGB15(0, 0, 0);
     PrintConsole *tmp = consoleSelect(console);
     consoleCls('2');
     consoleSelect(tmp);

include/nds/arm9/console.h
@@ -69,7 +69,6 @@ typedef struct ConsoleFont
     u8 bpp;          ///< Bits per pixel in the font graphics
     u16 asciiOffset; ///< Offset to the first valid character in the font table
     u16 numChars;    ///< Number of characters in the font graphics
-    bool convertSingleColor; ///< Convert from 1bpp font
 } ConsoleFont;
 
 /// Console structure used to store the state of a console render context.

include/nds/bios.h
@@ -263,10 +263,10 @@ static inline int swiIsDebugger(void)
 /// @param destination Destination address (word aligned).
 /// @param params Pointer to an UnpackStruct.
 __attribute__((always_inline))
-static inline void swiUnpackBits(const uint8_t *source, uint32_t *destination, TUnpackStruct *params)
+static inline void swiUnpackBits(const void *source, void *destination, TUnpackStruct *params)
 {
-    register const uint8_t* r0 asm("r0") = source;
-    register uint32_t* r1 asm("r1") = destination;
+    register const void* r0 asm("r0") = source;
+    register void* r1 asm("r1") = destination;
     register const TUnpackStruct* r2 asm("r2") = params;
     asm volatile inline ("swi 0x10 << ((1f - . == 4) * -16); 1:" : "+r"(r0), "+r"(r1), "+r"(r2) :: "r3", "memory");
 }

commit 320d2dc35693ae8a544822e3414ac8d503b283a0

console: Don't use BIOS functions in the console
This is a regression caused by commit e70ec2576779 ("console: clean up
consoleLoadFont()"). It was changed as a way to reduce the code
footprint of the function, and we didn't realize that it would break the
default exception handler. A comment has been added so that we are
careful in the future.

source/arm9/console.c
@@ -5,13 +5,16 @@
 // Copyright (C) 2005 Jason Rogers (dovoto)
 // Copyright (C) 2005 Dave Murphy (WinterMute)
 
+// Important note: We can't use any BIOS functions in this file because the
+// console is used by the default exception handler. The handler doesn't run in
+// CPU user mode, so it doesn't work correctly if it uses BIOS functions.
+
 #include <stdarg.h>
 #include <stdio.h>
 
 #include <nds/arm9/background.h>
 #include <nds/arm9/console.h>
 #include <nds/arm9/video.h>
-#include <nds/bios.h>
 #include <nds/debug.h>
 #include <nds/memory.h>
 #include <nds/ndstypes.h>
@@ -327,16 +330,50 @@ void consoleLoadFont(PrintConsole *console)
 
     console->fontCurPal <<= 12;
 
-    if (console->font.bpp <= 2)
+    if (console->font.bpp == 1)
     {
-        TUnpackStruct config = {
-            console->font.numChars * 8 * console->font.bpp,
-            console->font.bpp,
-            4,
-            16 - (1 << console->font.bpp)
-        };
+        for (int i = 0; i < console->font.numChars * 8; i++)
+        {
+            u8 row = ((const u8 *)console->font.gfx)[i];
+            u32 temp = 0;
 
-        swiUnpackBits(console->font.gfx, console->fontBgGfx, &config);
+            if (row & 0x01)
+                temp |= 0xF;
+            if (row & 0x02)
+                temp |= 0xF0;
+            if (row & 0x04)
+                temp |= 0xF00;
+            if (row & 0x08)
+                temp |= 0xF000;
+            if (row & 0x10)
+                temp |= 0xF0000;
+            if (row & 0x20)
+                temp |= 0xF00000;
+            if (row & 0x40)
+                temp |= 0xF000000;
+            if (row & 0x80)
+                temp |= 0xF0000000;
+
+            ((u32 *)console->fontBgGfx)[i] = temp;
+        }
+    }
+    else if (console->font.bpp == 2)
+    {
+        for (int i = 0; i < console->font.numChars * 16; i++)
+        {
+            u16 temp = 0;
+
+            if (console->font.gfx[i] & 0xF)
+                temp |= 0xF;
+            if (console->font.gfx[i] & 0xF0)
+                temp |= 0xF0;
+            if (console->font.gfx[i] & 0xF00)
+                temp |= 0xF00;
+            if (console->font.gfx[i] & 0xF000)
+                temp |= 0xF000;
+
+            console->fontBgGfx[i] = temp;
+        }
     }
     else if (console->font.bpp == 4)
     {