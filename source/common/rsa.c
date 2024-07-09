6f786835b0affb9ea27990c017a2d3387ed3df1c
Michele Di Giorgio <michele.dg90@gmail.com>
2024-07-09 22:05:17 +1000

    rsa: fix RSA functions paramaters
    The destination length in swiRSADecryptRAW needs to be a pointer a
    32-bit unsigned integer.
    
    A wrapper function to create the heap context is also provided to make
    the API more user-friendly.
    
    The patch also provides documentation for functions, paramaters and
    structures.

--- source/common/rsa.c
+++ source/common/rsa.c
@@ -6,22 +6,24 @@
 #include <nds/rsa.h>
 #include <nds/system.h>
 
+#include <stdlib.h>
+
 int swiRSAInitHeapTWL(swiRSAHeapContext_t *ctx, void *heapStart, size_t heapSize);
-int swiRSADecryptRAWTWL(swiRSAHeapContext_t *ctx, swiRSAbuffers_t *rsabuffers, size_t len);
+int swiRSADecryptRAWTWL(swiRSAHeapContext_t *ctx, swiRSAbuffers_t *rsabuffers, size_t *lenDst);
 int swiRSADecryptTWL(swiRSAHeapContext_t *ctx, void *dst, const void *sig, const void *key);
 int swiRSADecryptPGPTWL(swiRSAHeapContext_t *ctx, void *dst, const void *sig, const void *key);
 
 int swiRSAInitHeap(swiRSAHeapContext_t *ctx, void *heapStart, size_t heapSize)
 {
     if (isDSiMode())
-        return swiRSAInitHeapTWL(ctx, heapStart, heapSize);
+        return swiRSAInitHeapTWL(ctx, heapStart, heapSize) > 0;
     return 0;
 }
 
-int swiRSADecryptRAW(swiRSAHeapContext_t *ctx, swiRSAbuffers_t *rsabuffers, size_t len)
+int swiRSADecryptRAW(swiRSAHeapContext_t *ctx, swiRSAbuffers_t *rsabuffers, size_t *lenDst)
 {
     if (isDSiMode())
-        return swiRSADecryptRAWTWL(ctx, rsabuffers, len);
+        return swiRSADecryptRAWTWL(ctx, rsabuffers, lenDst);
     return 0;
 }
 
