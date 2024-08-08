1c789d64886c9c3654d61ca922e8b81348405091
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-08-08 18:26:02 +0100

    slot2: Fix code that detects DSi debugger RAM
    When the code was created the MPU didn't distinguish between retail and
    debugger models of DSi, so all the 32 MB of potential RAM could be
    accessed without triggering an exception. Also, isHwDebugger() didn't
    exist at the time.
    
    Since the creation of this code, the MPU setup code now looks for the
    debugger extended RAM and isHwDebugger() has been created to detect
    debugger models in a reliable way. However, the MPU now blocks accesses
    to the extended RAM if the console isn't a debugger unit (so that it's
    easier to detect bugs in your code).
    
    This patch simply switches to using isHwDebugger(). We know that if we
    reach this function we're in a DSi because that is checked in the only
    place where slot2DetectTWLDebugRam() is used.

--- source/arm9/peripherals/slot2.twl.c
+++ source/arm9/peripherals/slot2.twl.c
@@ -1,35 +1,16 @@
 // SPDX-License-Identifier: Zlib
 //
 // Copyright (c) 2023 Adrian "asie" Siekierka
+// Copyright (c) 2024 Antonio Niño Díaz
 
 #include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <nds/ndstypes.h>
-#include <nds/memory.h>
-#include <nds/system.h>
-#include <nds/arm9/peripherals/slot2.h>
 
-// Points to ARM9 ROM size in the in-memory NDS header copy
-#define TWL_RAM_TEST1 *((volatile uint32_t*) 0xCFFFE2C)
-#define TWL_RAM_TEST2 *((volatile uint32_t*) 0xDFFFE2C)
+#include <nds/system.h>
 
 bool slot2DetectTWLDebugRam(void)
 {
-    bool result = false;
+    if (isHwDebugger())
+        return true;
 
-    // Configure faux Slot-2 RAM
-    REG_SCFG_EXT = REG_SCFG_EXT | SCFG_EXT_RAM_DEBUG | SCFG_EXT_RAM_TWL;
-    // Temporarily change a safe-to-change value
-    uint32_t old_test1 = TWL_RAM_TEST1;
-    uint32_t old_test2 = TWL_RAM_TEST2;
-    TWL_RAM_TEST1 = 0x0000;
-    TWL_RAM_TEST2 = 0xFFFF;
-    if (TWL_RAM_TEST1 == 0x0000 && TWL_RAM_TEST2 == 0xFFFF)
-    {
-        result = true;
-    }
-    TWL_RAM_TEST1 = old_test1;
-    TWL_RAM_TEST2 = old_test2;
-    return result;
+    return false;
 }
