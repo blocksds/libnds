e7ed6cdf7c4b04e0ccc431b458baf293b4c8307f
Adrian Siekierka <kontakt@asie.pl>
2024-11-24 08:58:43 +0100

    fatfs: Update to R0.15a.

--- source/arm9/libc/fatfs/ffconf.h
+++ source/arm9/libc/fatfs/ffconf.h
@@ -18,7 +18,7 @@
 /
 /------------------------------------------------------------------------*/
 
-#define FFCONF_DEF	80286	/* Revision ID */
+#define FFCONF_DEF	5380	/* Revision ID */
 
 /*---------------------------------------------------------------------------/
 / Function Configurations
@@ -73,8 +73,8 @@
 
 
 #define FF_USE_STRFUNC	0
-#define FF_PRINT_LLI	1
-#define FF_PRINT_FLOAT	1
+#define FF_PRINT_LLI	0
+#define FF_PRINT_FLOAT	0
 #define FF_STRF_ENCODE	3
 /* FF_USE_STRFUNC switches string functions, f_gets(), f_putc(), f_puts() and
 /  f_printf().
