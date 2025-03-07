441df5f2b5c81c2ab61a3c34c3d7a81eabd69654
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-03-07 02:08:31 +0000

    sbrk: Always use fake heap pointers
    The `register uintptr_t stack_ptr asm("sp");` trick doesn't really work in
    all cases on the ARM7, so it's better to refactor all this code to work
    the same way and to expect the crt0 of both CPUs to set the values of
    fake_heap_start and fake_heap_end.

--- source/common/libc/sbrk.c
+++ source/common/libc/sbrk.c
@@ -1,55 +1,38 @@
 // SPDX-License-Identifier: Zlib
 //
-// Copyright (c) 2023 Antonio Niño Díaz
+// Copyright (c) 2023-2025 Antonio Niño Díaz
 
 #include <errno.h>
 #include <stddef.h>
 #include <stdint.h>
 
 #ifdef ARM9
-#define USE_FAKE_HEAP_BOUNDS
+#include <nds/arm9/sassert.h>
 #endif
 
-#ifdef USE_FAKE_HEAP_BOUNDS
 void *fake_heap_end = NULL;
 void *fake_heap_start = NULL;
-#endif
+
+// Next address to be used. It is updated after every call to sbrk()
+static uintptr_t heap_start = 0;
 
 void *sbrk(int incr)
 {
-    // Symbol defined by the linker
-    extern char __end__[];
-    const uintptr_t end = (uintptr_t)__end__;
-
-    // Trick to get the current stack pointer
-    register uintptr_t stack_ptr asm("sp");
-
-    // Next address to be used. It is updated after every call to sbrk()
-    static uintptr_t heap_start = 0;
-
-#ifdef USE_FAKE_HEAP_BOUNDS
+    // Get heap start
     if (heap_start == 0)
     {
-        if (fake_heap_start == NULL)
-            heap_start = end;
-        else
-            heap_start = (uintptr_t)fake_heap_start;
+#ifdef ARM9
+        sassert(fake_heap_start != NULL, "Fake heap not initialized");
+#endif
+        heap_start = (uintptr_t)fake_heap_start;
     }
 
-    // Current limit
-    uintptr_t heap_end;
-
-    if (fake_heap_end == NULL)
-        heap_end = stack_ptr;
-    else
-        heap_end = (uintptr_t)fake_heap_end;
-#else
-    if (heap_start == 0)
-        heap_start = end;
-
-    // Current limit
-    uintptr_t heap_end = stack_ptr;
+#ifdef ARM9
+    sassert(fake_heap_end != NULL, "Fake heap not initialized");
 #endif
+
+    // Get heap end
+    uintptr_t heap_end = (uintptr_t)fake_heap_end;
 
     // Try to allocate
     if (heap_start + incr > heap_end)
