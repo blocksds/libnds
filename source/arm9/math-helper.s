13c68b6a3c04746a3b59d60fc2bf3be86afcb43b
Kuratius <Kuratius@gmx.net>
2025-05-28 14:10:32 +0200

    remove spaces

--- source/arm9/math-helper.s
+++ source/arm9/math-helper.s
@@ -5,11 +5,11 @@
 .global start_div64_64
 .type   start_div64_64, %function
 start_div64_64:
-    mov r12, #0x04000000 ;
+    mov r12, #0x04000000;
     add r12, r12, #0x290;
     stmia r12, {r0-r3};
     ldr r0, [r12, #-0x10];
-    mov r1, #2; 
+    mov r1, #2;
     and r0, r0, #3;
     cmp r0, #2;
     strne r1, [r12, #-0x10];
@@ -26,7 +26,6 @@ finish_div64_64:
 1:
     tst r0, #0x8000;
     ldrne r0, [r1, #-0x10];
-    bne 1b ;
+    bne 1b;
     ldrd r0, r1,[r1, #0x10];
     bx lr
-    
