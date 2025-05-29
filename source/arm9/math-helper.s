29d39936f52363b2726a3a1b05adf0f708a14bfe
Kuratius <Kuratius@gmx.net>
2025-05-29 16:45:47 +0200

    license identifier

--- source/arm9/math-helper.s
+++ source/arm9/math-helper.s
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (C) 2024 Dominik Kurz
+
 .syntax  unified
 .arm
 .text
-.balign 4
-.global start_div64_64
-.type   start_div64_64, %function
-start_div64_64:
+BEGIN_ASM_FUNC start_div64_64
     mov r12, #0x04000000;
     add r12, r12, #0x290;
     stmia r12, {r0-r3};
@@ -16,10 +17,7 @@ start_div64_64:
     bx lr;
 .arm
 .text
-.balign 4
-.global finish_div64_64
-.type finish_div64_64, %function
-finish_div64_64:
+BEGIN_ASM_FUNC finish_div64_64
     mov r1, #0x04000000;
     add r1, r1, #0x290;
     ldr r0, [r1, #-0x10];
