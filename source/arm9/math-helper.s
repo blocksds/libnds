463b8661ce9aa0e3b84063b4be2aaec30df93f72
Kuratius <Kuratius@gmx.net>
2025-05-31 16:52:36 +0200

    comment

--- source/arm9/math-helper.s
+++ source/arm9/math-helper.s
@@ -24,7 +24,7 @@ BEGIN_ASM_FUNC finish_div64_64
     add r1, r1, #0x290;
     ldr r0, [r1, #-0x10];   @load REG_DIV_CNT
 1:
-    tst r0, #0x8000;        @check whether result is ready
+    tst r0, #0x8000;        @check DIV_BUSY bit
     ldrne r0, [r1, #-0x10]; @load REG_DIV_CNT
     bne 1b;
     ldrd r0, r1,[r1, #0x10];@load result from REG_DIV_RESULT
