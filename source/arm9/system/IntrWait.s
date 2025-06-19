5cbcf0f3666997cc437a76fdd8ea08df6965753a
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-06-19 01:11:27 +0100

    swi: Fix implementation of swiIntrWait() in libnds
    The old implementation was buggy (for example, sometimes it used r12 as
    base of the I/O registers when it was actually set to 0).

--- source/arm9/system/IntrWait.s
+++ source/arm9/system/IntrWait.s
@@ -2,6 +2,7 @@
 // SPDX-FileNotice: Modified from the original version by the BlocksDS project.
 //
 // Copyright (C) 2005-2016 Dave Murphy (WinterMute)
+// Copyright (C) 2025 Antonio Niño Díaz
 
 #include <nds/asminc.h>
 #include <nds/arm9/cp15_asm.h>
@@ -21,28 +22,43 @@ BEGIN_ASM_FUNC swiWaitForVBlank
 
 BEGIN_ASM_FUNC_NO_SECTION swiIntrWait
 
-    push    {lr}
-
     // Disable interrupts (REG_IME = 0)
     mov     r12, #0x4000000
     str     r12, [r12, #0x208]
 
-    // Get the DTCM base address
-    mrc     CP15_REG9_DTCM_CONTROL(r3)
-    mov     r3, r3, lsr #12 // Clear the lower 12 bits
-    mov     r3, r3, lsl #12
-    // Calculate the DTCM end address
-    add     r3, r3, #0x4000
+    // Prepare a pointer to the BIOS IRQ flags
+    ldr     r3, =__irq_flags
 
-    mov     r12, r0
-    bl      check_flags
-    beq     wait
+    // Check mode
+    cmp     r0, #0
+    beq     mode_return_if_flags_set
 
-    cmp     r12, #0
-    beq     flag_set
-wait:
-    mov     r12, #0x4000000
-wait_flags:
+    // r0 = 1 : Discard old flags, wait until a new flag becomes set
+
+mode_discard_old_flags:
+
+    // Discard old flags
+    ldr     r2, [r3]
+    bic     r2, r2, r1
+    str     r2, [r3]
+
+    b       wait_loop
+
+mode_return_if_flags_set:
+
+    // r0 = 0 : Return immediately if an old flag was already set
+
+    // Check flags and clear them if they are set
+    ldr     r2, [r3]
+    ands    r0, r1, r2
+    eorne   r2, r2, r0
+    strne   r2, [r3]
+    // Exit if there was a flag already set
+    bne     exit
+
+    // Wait loop. Common for both modes
+wait_loop:
+
     // Enable interrupts
     mov     r2, #1
     str     r2, [r12, #0x208]
@@ -51,20 +67,20 @@ wait_flags:
     mcr     CP15_REG7_WAIT_FOR_INTERRUPT
 
     // Disable interrupts
-    str     r12, [r12, #0x208]
-    bl      check_flags
-    beq     wait_flags
+    str     r0, [r12, #0x208]
 
-flag_set:
-    // Enable interrupts
-    mov     r2, #1
-    str     r2, [r12, #0x208]
-    pop     {pc}
-
-check_flags:
-    // The BIOS flags are stored 8 bytes before the end of DTCM
-    ldr     r2, [r3, #-8]
+    // Check flags and clear them if they are set
+    ldr     r2, [r3]
     ands    r0, r1, r2
     eorne   r2, r2, r0
-    strne   r2, [r3, #-8]
+    strne   r2, [r3]
+
+    beq     wait_loop
+
+exit:
+    // Enable interrupts
+    mov     r12, #0x4000000
+    mov     r2, #1
+    str     r2, [r12, #0x208]
+
     bx      lr
