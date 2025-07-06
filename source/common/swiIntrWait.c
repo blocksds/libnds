a8577dcd39f31a30da104909961d4f60e4959646
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-06 23:32:21 +0100

    irq: Implement swiIntrWait() and swiIntrWaitAUX() in C
    The reason why the BIOS has bugs in those functions is that they are
    actually quite tricky to get right. Let's just write them in C so that it's
    easier to understand the code and to have it bug-free, and let the compiler
    generate the assembly code for us.
    
    After checking the generated code, it seems to be reasonably short and
    fast.
    
    This patch also moves the ARM9 functions to ITCM.

---
+++ source/common/swiIntrWait.c
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: Zlib
+//
+// Copyright (C) 2025 Antonio Niño Díaz
+
+// This implements some BIOS functions that are buggy in the real BIOS. The bugs
+// depend on the CPU and the NDS model, so it's better to reimplement them
+// ourselves. The compiler does a pretty good job, so it's better to have them
+// as C than as hand-written ASM.
+
+#include <nds/bios.h>
+#include <nds/interrupts.h>
+
+#pragma GCC optimize ("O3")
+
+ARM_CODE ITCM_CODE
+void swiWaitForVBlank(void)
+{
+    swiIntrWait(INTRWAIT_CLEAR_FLAGS, IRQ_VBLANK);
+}
+
+__attribute__((noinline)) // So that it isn't inlined in swiWaitForVBlank()
+ARM_CODE ITCM_CODE
+void swiIntrWait(u32 clearOldFlags, uint32_t flags)
+{
+    REG_IME = 0;
+
+    vuint32 *bios_flags = &__irq_flags[0];
+
+    if (clearOldFlags == INTRWAIT_CLEAR_FLAGS)
+        *bios_flags &= ~flags;
+
+    while (1)
+    {
+        uint32_t old_flags = *bios_flags;
+        uint32_t irqs_found = old_flags & flags;
+        *bios_flags = old_flags & ~irqs_found;
+
+        if (irqs_found)
+            break;
+
+        REG_IME = 1;
+
+#ifdef ARM9
+        // Any register works for this as long as its value is zero
+        asm volatile inline (
+            "mcr p15, 0, %0, c7, c0, 4\n\t" // CP15_REG7_WAIT_FOR_INTERRUPT
+            :           // Outputs
+            : "r"(0)    // Inputs
+            : "memory"  // Clobber list. The values of the BIOS registers may change
+        );
+#else
+        swiHalt();
+#endif
+
+        REG_IME = 0;
+    }
+
+    REG_IME = 1;
+}
+
+#ifdef ARM7
+ARM_CODE
+void swiIntrWaitAUX(u32 clearOldFlags, uint32_t flags, uint32_t aux_flags)
+{
+    REG_IME = 0;
+
+    vuint32 *bios_flags = &__irq_flags[0];
+    vuint32 *bios_flags_aux = &__irq_flagsaux[0];
+
+    if (clearOldFlags == INTRWAIT_CLEAR_FLAGS)
+    {
+        *bios_flags &= ~flags;
+        *bios_flags_aux &= ~aux_flags;
+    }
+
+    while (1)
+    {
+        uint32_t old_flags = *bios_flags;
+        uint32_t irqs_found = old_flags & flags;
+        *bios_flags = old_flags & ~irqs_found;
+
+        uint32_t old_flags_aux = *bios_flags_aux;
+        uint32_t irqs_found_aux = old_flags_aux & aux_flags;
+        *bios_flags_aux = old_flags_aux & ~irqs_found_aux;
+
+        if (irqs_found | irqs_found_aux)
+            break;
+
+        REG_IME = 1;
+        swiHalt();
+        REG_IME = 0;
+    }
+
+    REG_IME = 1;
+}
+#endif // ARM7
