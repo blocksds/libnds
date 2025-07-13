f653e51d0f352ea9bd3aee141b59d8d500256aa9
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-13 11:12:09 +0100

    cothread: Verify that TLS isn't used from IRQ handlers
    This check is only available in debug builds of libnds for now.

--- source/common/cothread/aeabi_read_tp.s
+++ source/common/cothread/aeabi_read_tp.s
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Zlib
 //
-// Copyright (C) 2023 Antonio Niño Díaz
+// Copyright (C) 2023-2025 Antonio Niño Díaz
 
 #include <nds/asminc.h>
 
@@ -14,11 +14,38 @@
 // written in Thumb. Also, there is no point in placing this code in ITCM
 // because any code located in main RAM will need to call a veneer to jump to
 // this routine. It's better to place it in regular RAM.
+//
+// Note: Using TLS from an interrupt handler is problematic. The code uses the
+// TLS area of the thread that was interrupted, which may cause weird issues.
+// Even when interrupts are nested, the same TLS area is used. In debug builds
+// we need to check if this function is called from an interrupt handler to
+// detect this situation. The check makes the function twice as slow, so it
+// isn't a good idea to keep it in release builds for now.
 
     .syntax  unified
     .thumb
 
 BEGIN_ASM_FUNC __aeabi_read_tp
+
+#ifndef NDEBUG
+    ldr     r0, =irq_nesting_level
+    ldr     r0, [r0]
+    cmp     r0, #0
+    beq     not_in_irq
+
+    // Jump to libndsCrash(), which should never return
+    ldr     r0, =__aeabi_read_tp_error_msg
+    ldr     r1, =libndsCrash
+    bx      r1
+
+    // libndsCrash() never returns, it's safe to place the message here
+__aeabi_read_tp_error_msg:
+    .asciz "Using TLS from IRQ"
+    .align  2
+
+not_in_irq:
+#endif
+
     ldr     r0, =__tls
     ldr     r0, [r0]
     bx      lr
