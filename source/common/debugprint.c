16cd07a31d16d164c4833845f283dde430a5e0c9
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-10-03 17:59:45 +0100

    debug: Simplify no$gba message print on the ARM9
    melonDS currently doesn't support the debug registers on the ARM7.

--- source/common/debugprint.c
+++ source/common/debugprint.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Zlib
 //
-// Copyright (C) 2023 Antonio Niño Díaz
+// Copyright (C) 2023-2025 Antonio Niño Díaz
 
 #include <stdio.h>
 
@@ -8,6 +8,33 @@
 
 #include "common/libnds_internal.h"
 
+// TODO: melonDS doesn't support the debug registers on the ARM7 for now. When
+// it supports them we can switch both CPUs to the ARM9 version of this
+// function.
+
+#ifdef ARM9
+#define NOCASHGBA_BUFFER_SIZE 120
+static char nocash_buf[NOCASHGBA_BUFFER_SIZE + 1]; // Leave a trailing zero
+static uint8_t nocash_buf_len = 0;
+
+int nocash_putc_buffered(char c, FILE *file)
+{
+    (void)file;
+
+    nocash_buf[nocash_buf_len] = c;
+    nocash_buf_len++;
+
+    if ((c == '\n') || (nocash_buf_len == NOCASHGBA_BUFFER_SIZE))
+    {
+        REG_NOCASH_STR_PARAM = (uintptr_t)&nocash_buf;
+        nocash_buf_len = 0;
+    }
+
+    return c;
+}
+#endif // ARM9
+
+#ifdef ARM7
 #define NOCASHGBA_BUFFER_SIZE 120
 static char nocash_buf[NOCASHGBA_BUFFER_SIZE];
 static uint8_t nocash_buf_len = 0;
@@ -16,6 +43,7 @@ int nocash_putc_buffered(char c, FILE *file)
 {
     (void)file;
 
+    // Don't send '\n' characters to nocashWrite(), it adds them by default.
     if (c != '\n')
     {
         nocash_buf[nocash_buf_len] = c;
@@ -30,3 +58,4 @@ int nocash_putc_buffered(char c, FILE *file)
 
     return c;
 }
+#endif
