3b61606f132cd4ff9462bb006994af20463dcff5
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-12-23 00:32:52 +0100

    asm: Allow setting the architecture name in BEGIN_ASM_FUNC*
    This allows the developer to save a few bytes by padding only to 2 bytes
    in Thumb functions instead of 4.

--- include/nds/asminc.h
+++ include/nds/asminc.h
@@ -2,6 +2,7 @@
 // SPDX-FileNotice: Modified from the original version by the BlocksDS project.
 //
 // Copyright (C) 2017-2018 Dave Murphy (WinterMute)
+// Copyright (C) 2025 Antonio Niño Díaz
 
 #ifndef LIBNDS_ASMINC_H__
 #define LIBNDS_ASMINC_H__
@@ -10,21 +11,47 @@
 # error "This header file is only for use in assembly files!"
 #endif
 
-.macro BEGIN_ASM_FUNC name section=text
+.macro BEGIN_ASM_FUNC name section=text arch=unset
     .section .\section\().\name\(), "ax", %progbits
     .global \name
     .type \name, %function
-    .align 2
+
+    .if \arch == thumb
+        .align          1
+        .thumb
+    .elseif \arch == arm
+        .align          2
+        .arm
+    .elseif \arch == unset
+        // If the architecture it isn't specified, don't set it in this macro.
+        // Set the aligment to the one that will work in all cases.
+        .align          2
+    .else
+        .error "Unknown architecture"
+    .endif
 \name:
 .endm
 
 // Remember to use this macro if one function falls through the following one.
 // If not, the linker may choose to remove the second one if it isn't referenced
 // anywhere else.
-.macro BEGIN_ASM_FUNC_NO_SECTION name
+.macro BEGIN_ASM_FUNC_NO_SECTION name arch=unset
     .global \name
     .type \name, %function
-    .align 2
+
+    .if \arch == thumb
+        .align          1
+        .thumb
+    .elseif \arch == arm
+        .align          2
+        .arm
+    .elseif \arch == unset
+        // If the architecture it isn't specified, don't set it in this macro.
+        // Set the aligment to the one that will work in all cases.
+        .align          2
+    .else
+        .error "Unknown architecture"
+    .endif
 \name:
 .endm
 
