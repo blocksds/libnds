af78288b525aec2d47e5d36246a74b0a342b04b9
Adrian Siekierka <kontakt@asie.pl>
2026-01-05 13:34:01 +0100

    dlmalloc: Re-introduce a global lock.

--- source/common/dlmalloc/dlmalloc.c
+++ source/common/dlmalloc/dlmalloc.c
@@ -2,7 +2,11 @@
 //
 // Copyright (C) 2026 Adrian "asie" Siekierka
 
+#include <nds/cothread.h>
+
 #pragma GCC optimize("-Os")
+
+// dlmalloc configuration
 
 #ifdef DEBUG
 # define FOOTERS 1
@@ -15,6 +19,30 @@
 #define HAVE_MORECORE 1
 #define HAVE_MMAP 0
 #define malloc_getpagesize 4096
+
+// Global lock implementation
+
+#define USE_LOCKS 2
+
+static inline int ACQUIRE_LOCK(comutex_t *lk)
+{
+    comutex_acquire(lk);
+    return 0;
+}
+
+static inline int RELEASE_LOCK(comutex_t *lk)
+{
+    comutex_release(lk);
+    return 0;
+}
+
+#define MLOCK_T comutex_t
+static comutex_t malloc_global_mutex;
+#define INITIAL_LOCK(lk) comutex_init((lk))
+#define DESTROY_LOCK(lk) {}
+#define TRY_LOCK(lk) (comutex_try_acquire((lk))?0:1)
+
+// malloc implementations and aliases
 
 #undef DEBUG
 #include "dlmalloc_impl.h"
