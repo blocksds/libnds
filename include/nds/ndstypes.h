ee8907338c35f82f47d342edf69fa98cc7676b4c
PoroCYon <p@pcy.be>
2026-03-13 14:54:17 +0100

    ndstypes: Surround attribute names with double-underscores to prevent collisions with macro names

--- include/nds/ndstypes.h
+++ include/nds/ndstypes.h
@@ -62,71 +62,71 @@ extern "C" {
 
 
 /// Used to place a function in ITCM
-#define ITCM_CODE __attribute__((section(".itcm.text." __FILE__ "." LIBNDS_STRINGIFY(__LINE__)), long_call))
+#define ITCM_CODE __attribute__((__section__(".itcm.text." __FILE__ "." LIBNDS_STRINGIFY(__LINE__)), __long_call__))
 /// Used to place a function in ITCM
-#define ITCM_FUNC(x) __attribute__((section(".itcm.text." LIBNDS_STRINGIFY(x)))) x
+#define ITCM_FUNC(x) __attribute__((__section__(".itcm.text." LIBNDS_STRINGIFY(x)))) x
 
 /// Used to place initialized data in ITCM
-#define ITCM_DATA __attribute__((section(".itcm.data." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define ITCM_DATA __attribute__((__section__(".itcm.data." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place initialized data in ITCM
-#define ITCM_DATA_VAR(x) __attribute__((section(".itcm.data." LIBNDS_STRINGIFY(x)))) x
+#define ITCM_DATA_VAR(x) __attribute__((__section__(".itcm.data." LIBNDS_STRINGIFY(x)))) x
 
 /// Used to place uninitialized data in ITCM
-#define ITCM_BSS  __attribute__((section(".itcm.bss." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define ITCM_BSS  __attribute__((__section__(".itcm.bss." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place uninitialized data in ITCM
-#define ITCM_BSS_VAR(x) __attribute__((section(".itcm.bss." LIBNDS_STRINGIFY(x)))) x
+#define ITCM_BSS_VAR(x) __attribute__((__section__(".itcm.bss." LIBNDS_STRINGIFY(x)))) x
 
 /// Used to place initialized data in DTCM
-#define DTCM_DATA __attribute__((section(".dtcm." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define DTCM_DATA __attribute__((__section__(".dtcm." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place uninitialized data in DTCM
-#define DTCM_BSS __attribute__((section(".sbss." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define DTCM_BSS __attribute__((__section__(".sbss." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place initialized data in DTCM
-#define DTCM_DATA_VAR(x) __attribute__((section(".dtcm." LIBNDS_STRINGIFY(x)))) x
+#define DTCM_DATA_VAR(x) __attribute__((__section__(".dtcm." LIBNDS_STRINGIFY(x)))) x
 /// Used to place uninitialized data in DTCM
-#define DTCM_BSS_VAR(x) __attribute__((section(".sbss." LIBNDS_STRINGIFY(x)))) x
+#define DTCM_BSS_VAR(x) __attribute__((__section__(".sbss." LIBNDS_STRINGIFY(x)))) x
 
 /// Used to place a function in DSi RAM.
-#define TWL_CODE __attribute__((section(".twl.text." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define TWL_CODE __attribute__((__section__(".twl.text." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place initialized data in DSi RAM.
-#define TWL_DATA __attribute__((section(".twl.data." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define TWL_DATA __attribute__((__section__(".twl.data." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place uninitialized data in DSi RAM.
-#define TWL_BSS __attribute__((section(".twl_bss." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
+#define TWL_BSS __attribute__((__section__(".twl_bss." __FILE__ "." LIBNDS_STRINGIFY(__LINE__))))
 /// Used to place a function in DSi RAM.
-#define TWL_FUNC(x) __attribute__((section(".twl.text." LIBNDS_STRINGIFY(x)))) x
+#define TWL_FUNC(x) __attribute__((__section__(".twl.text." LIBNDS_STRINGIFY(x)))) x
 /// Used to place initialized data in DSi RAM.
-#define TWL_DATA_VAR(x) __attribute__((section(".twl.data." LIBNDS_STRINGIFY(x)))) x
+#define TWL_DATA_VAR(x) __attribute__((__section__(".twl.data." LIBNDS_STRINGIFY(x)))) x
 /// Used to place uninitialized data in DSi RAM.
-#define TWL_BSS_VAR(x) __attribute__((section(".twl_bss." LIBNDS_STRINGIFY(x)))) x
+#define TWL_BSS_VAR(x) __attribute__((__section__(".twl_bss." LIBNDS_STRINGIFY(x)))) x
 
 /// Used to tell the compiler to compile a function as ARM code
-#define ARM_CODE __attribute__((target("arm")))
+#define ARM_CODE __attribute__((__target__("arm")))
 /// Used to tell the compiler to compile a function as Thumb code (default)
-#define THUMB_CODE __attribute__((target("thumb")))
+#define THUMB_CODE __attribute__((__target__("thumb")))
 
 /// Aligns a struct (and other types) to "m".
-#define ALIGN(m) __attribute__((aligned(m)))
+#define ALIGN(m) __attribute__((__aligned__(m)))
 
 /// Packs a struct so it won't include padding bytes.
-#define PACKED __attribute__ ((packed))
+#define PACKED __attribute__ ((__packed__))
 #define packed_struct struct PACKED
 
 /// Used to mark functions and types as deprecated.
-#define LIBNDS_DEPRECATED __attribute__((deprecated))
+#define LIBNDS_DEPRECATED __attribute__((__deprecated__))
 
 /// Used to mark functions that mustn't be inlined.
-#define LIBNDS_NOINLINE __attribute__((noinline))
+#define LIBNDS_NOINLINE __attribute__((__noinline__))
 
 /// Used to mark functions that must be inlined.
-#define LIBNDS_ALWAYS_INLINE __attribute__((always_inline))
+#define LIBNDS_ALWAYS_INLINE __attribute__((__always_inline__))
 
 /// Used to mark functions that don't return to the caller.
-#define LIBNDS_NORETURN __attribute__((noreturn))
+#define LIBNDS_NORETURN __attribute__((__noreturn__))
 
 /// Used to mark functions that expect strings formatted like printf().
-#define LIBNDS_PRINTFLIKE(x, y) __attribute__((format(printf, x, y)))
+#define LIBNDS_PRINTFLIKE(x, y) __attribute__((__format__(printf, x, y)))
 
 /// Used to tag functions with arguments that can never be NULL
-#define LIBNDS_NONNULL(...) __attribute__((nonnull(__VA_ARGS__)))
+#define LIBNDS_NONNULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
 
 /// Helper that prevents the compiler from reordering memory accesses.
 ///
@@ -146,7 +146,7 @@ extern "C" {
 
 /// Makes the compiler output a warning if the return value of a function is
 /// ignored.
-#define WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+#define WARN_UNUSED_RESULT __attribute__((__warn_unused_result__))
 
 #endif // __INTELLISENSE__
 
