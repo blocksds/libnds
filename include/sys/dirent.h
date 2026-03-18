ab38b8566571d3addd0febed6f5558c9d6cb5ac3
Adrian Siekierka <kontakt@asie.pl>
2026-03-18 15:18:41 +0100

    libc: make <sys/dirent.h> more compatible with picolibc to fix C++ filesystem code

--- include/sys/dirent.h
+++ include/sys/dirent.h
@@ -10,37 +10,11 @@ extern "C" {
 #endif
 
 #include <sys/types.h>
+#include_next <sys/dirent.h>
 
-/// UTF-8 necessitates a maximum of three bytes for any UTF-16 codepoint.
-#define MAXNAMLEN (255 * 3)
+#define MAXNAMLEN NAME_MAX
 
-typedef unsigned short reclen_t;
-
-struct dirent
-{
-    /// Inode number. Implementation-defined.
-    ///
-    /// For FAT filesystems, this stores the cluster the file is located on.
-    /// For NitroFS filesystems, this stores the ID of the file.
-    ino_t d_ino;
-
-    /// Index within the directory.
-    off_t d_off;
-
-    /// File/directory name.
-    char d_name[MAXNAMLEN + 1];
-
-    /// File/directory type.
-    ///
-    /// For BlocksDS, this will typically be either DT_REG (file), or DT_DIR
-    /// (directory).
-    unsigned char d_type;
-
-    /// Size of this directory entry.
-    reclen_t d_reclen;
-};
-
-typedef struct
+struct __dirstream
 {
     // Buffer containing the returned directory entry.
     struct dirent dirent;
@@ -53,23 +27,7 @@ typedef struct
 
     // Type of native directory structure pointer.
     uint8_t dptype;
-} DIR;
-
-/// Unknown file type.
-#define DT_UNKNOWN 0
-
-/// Regular file.
-#define DT_REG 1
-
-/// Directory.
-#define DT_DIR 2
-
-// The following are not used by BlocksDS, but provided for compatibility.
-#define DT_FIFO 3
-#define DT_CHR 4
-#define DT_BLK 5
-#define DT_LNK 6
-#define DT_SOCK 7
+};
 
 #ifdef __cplusplus
 }
